df98bf521f328007e077c38ae2021ef937b3ebd6
Kotvuk <kotvuk@gmail.com>
2025-08-27 20:10:00 +0000

    add metrics middleware

---
+++ backend/middleware/metrics.js
@@ -0,0 +1,35 @@
+const client = require('prom-client');
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+const httpRequestsTotal = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total HTTP requests',
+  labelNames: ['method', 'route', 'status'],
+  registers: [register],
+});
+const httpRequestDuration = new client.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'HTTP request duration in milliseconds',
+  labelNames: ['method', 'route', 'status'],
+  buckets: [5, 10, 25, 50, 100, 250, 500, 1000, 2500, 5000],
+  registers: [register],
+});
+
+function metricsMiddleware(req, res, next) {
+  const start = Date.now();
+  res.on('finish', () => {
+    const duration = Date.now() - start;
+    const route = req.route ? req.route.path : req.path;
+    httpRequestsTotal.inc({ method: req.method, route, status: res.statusCode });
+    httpRequestDuration.observe({ method: req.method, route, status: res.statusCode }, duration);
+  });
+  next();
+}
+
+async function metricsEndpoint(req, res) {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+}
+
+module.exports = { metricsMiddleware, metricsEndpoint };
